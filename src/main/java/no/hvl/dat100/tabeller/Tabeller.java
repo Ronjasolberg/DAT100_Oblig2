316774258d3f6f490c7ce7c2d2560db4988f23fa
VildeKalsaas <vikal2986@hvl.no>
2025-09-26 12:32:54 +0200

    Update Tabeller.java

--- src/main/java/no/hvl/dat100/tabeller/Tabeller.java
+++ src/main/java/no/hvl/dat100/tabeller/Tabeller.java
@@ -53,16 +53,23 @@ public class Tabeller {
 
 	// e)
 	public static int posisjonTall(int[] tabell, int tall) {
-
-		// TODO
-		throw new UnsupportedOperationException("Metoden posisjonTall ikke implementert");
+        for (int i = 0; i < tabell.length; i++) {
+            if (tabell[i] == tall) {
+                return i;
+            }
+        }
+        return -1;
 	}
 
 	// f)
 	public static int[] reverser(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("Metoden reverser ikke implementert");
+        int[] nyTabell = new int [tabell.length];
+        int j = 0;
+        for(int i = tabell.length - 1; i >= 0; i--){
+            nyTabell[j] += tabell[i];
+            j++;
+        }
+        return nyTabell;
 	}
 
 	// g)
