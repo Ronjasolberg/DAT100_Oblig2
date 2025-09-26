87de52e1a877e8ee02a7bbe9439d8e146b0d0822
Ronjasolberg <ronjasolberg12@hotmail.com>
2025-09-26 12:13:45 +0200

    gjort oppgaver til og med e)

--- src/main/java/no/hvl/dat100/tabeller/Tabeller.java
+++ src/main/java/no/hvl/dat100/tabeller/Tabeller.java
@@ -2,41 +2,56 @@ package no.hvl.dat100.tabeller;
 
 public class Tabeller {
 
-	// a)
-	public static void skrivUt(int[] tabell) {
+    // a)
+    public static void skrivUt(int[] tabell) {
 
-		System.out.println(tabell);
+        System.out.println(tabell);
 
-	}
+    }
 
-	// b)
-	public static String tilStreng(int[] tabell) {
+    // b)
+    public static String tilStreng(int[] tabell) {
+        String resultat = "[";
+        for (int i = 0; i < tabell.length; i++) {
+            resultat = resultat + tabell[i];
+            if (i < tabell.length - 1) {
+                resultat = resultat + ",";
+            }
+        }
+        resultat = resultat + "]";
+        return resultat;
+    }
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden tilStreng ikke implementert");
-	}
+    // c)
+    public static int summer(int[] tabell) {
 
-	// c)
-	public static int summer(int[] tabell) {
+        int sum = 0;
+        for (int i = 0; i < tabell.length; i++) {
+            sum = sum + tabell[i];
+        }
+        return sum;
+    }
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden summer ikke implementert");
-	}
-
-	// d)
-	public static boolean finnesTall(int[] tabell, int tall) {
-
-		// TODO
-		throw new UnsupportedOperationException("Metoden finnesTall ikke implementert");
-
-	}
+    // d)
+    public static boolean finnesTall(int[] tabell, int tall) {
+        int i = 0;
+        while ( i < tabell.length) {
+            if (tabell[i] == tall) {
+                return true;
+            } i++;
+        } return false;
+    }
 
 	// e)
 	public static int posisjonTall(int[] tabell, int tall) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden posisjonTall ikke implementert");
-	}
+        for (int i = 0; i < tabell.length; i++) {
+            if (tabell[i] == tall) {
+                return i;
+            }
+            return -1;
+        }
+    }
 
 	// f)
 	public static int[] reverser(int[] tabell) {
