68a9cb00dde8a1677ea225842b5faa4ef01a4b53
VildeKalsaas <vikal2986@hvl.no>
2025-10-01 14:01:22 +0200

    oppg2

--- src/main/java/no/hvl/dat100/matriser/Matriser.java
+++ src/main/java/no/hvl/dat100/matriser/Matriser.java
@@ -14,10 +14,14 @@ public class Matriser {
 
 	// b)
 	public static String tilStreng(int[][] matrise) {
-
-		// TODO
-		throw new UnsupportedOperationException("Metoden tilStreng ikke implementert");
-		
+        String resultat = "";
+        for (int i = 0; i < matrise.length; i++){
+            for (int j = 0; j < matrise[i].length; j++){
+                resultat += matrise[i][j] + " ";
+            }
+            resultat += "\n";
+        }
+        return resultat;
 	}
 
 	// c)
